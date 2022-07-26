c3603ec1511ce75aa363051dc33d7a9ffbe47157
victordias <victoroliveirad96@gmail.com>
2022-07-26 20:27:13 -0300

    chore(fonts): configure fonts

--- App.tsx
+++ App.tsx
@@ -1,6 +1,16 @@
 import { Text, View } from 'react-native'
+import AppLoading from 'expo-app-loading'
+import { useFonts, DMSans_400Regular } from '@expo-google-fonts/dm-sans'
+import { DMSerifDisplay_400Regular } from '@expo-google-fonts/dm-serif-display'
 
 export default function App() {
+  const [fontsIsLoaded] = useFonts({
+    DMSans_400Regular,
+    DMSerifDisplay_400Regular
+  })
+
+  if (!fontsIsLoaded) return <AppLoading />
+
   return (
     <View>
       <Text>App izza</Text>
